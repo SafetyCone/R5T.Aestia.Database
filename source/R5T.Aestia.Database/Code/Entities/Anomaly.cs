ea4fa3ddb65839f11816a6c468e5d0fa7c5c49a7
MinexAutomation <minex.automation@gmail.com>
2019-11-25 11:18:02 -0800

    Made anomaly entity fields nullable.

--- source/R5T.Aestia.Database/Code/Entities/Anomaly.cs
+++ source/R5T.Aestia.Database/Code/Entities/Anomaly.cs
@@ -7,10 +7,10 @@ namespace R5T.Aestia.Database.Entities
     public class Anomaly
     {
         public int ID { get; set; }
-        public Guid GUID { get; set; }
-        public DateTime ReportedUTC { get; set; }
-        public Guid RepotedLocationGUID { get; set; }
-        public Guid ReporterLocationGUID { get; set; }
+        public Guid? GUID { get; set; }
+        public DateTime? ReportedUTC { get; set; }
+        public Guid? RepotedLocationGUID { get; set; }
+        public Guid? ReporterLocationGUID { get; set; }
 
         public ICollection<AnomalyToImageFileMapping> AnomalyToImageFileMappings { get; set; }
         public ICollection<AnomalyToTextItemMapping> AnomalyToTextItemMappings { get; set; }
