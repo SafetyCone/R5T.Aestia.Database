e81f27bb778119cc61faaaf2a29568381f3819d0
MinexAutomation <minex.automation@gmail.com>
2020-01-05 12:35:43 -0800

    Added IAnomalyDbContext extensions.

---
+++ source/R5T.Aestia.Database/Code/Extensions/IAnomalyDbContextExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+using R5T.Sindia;
+
+using AnomalyEntity = R5T.Aestia.Database.Entities.Anomaly;
+
+
+namespace R5T.Aestia.Database
+{
+    public static class IAnomalyDbContextExtensions
+    {
+        public static IQueryable<AnomalyEntity> GetAnomaly(this IAnomalyDbContext dbContext, Guid anomalyIdentityValue)
+        {
+            var anomalyEntityQueryable = dbContext.Anomalies.Where(x => x.GUID == anomalyIdentityValue);
+            return anomalyEntityQueryable;
+        }
+
+        public static IQueryable<AnomalyEntity> GetAnomaly(this IAnomalyDbContext dbContext, AnomalyIdentity anomalyIdentity)
+        {
+            var anomalyEntityQueryable = dbContext.GetAnomaly(anomalyIdentity.Value);
+            return anomalyEntityQueryable;
+        }
+    }
+}
